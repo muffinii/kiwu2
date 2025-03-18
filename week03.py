296242e7373e7029a4e53b853525feadc3bdd97e
inhadeepblue <deepblue@inha.ac.kr>
2025-03-18 19:40:35 +0900

    week03 kiosk v1.5 document

--- week03.py
+++ week03.py
@@ -9,6 +9,10 @@ total_price = 0
 # total_price = 0
 
 def order_process(idx: int):
+    """
+    Functions that address the beverage order display function, the total cumulative amount calculation function, and the beverage order quantity processing function
+    :param idx: list's index number
+    """
     global total_price
     print(f"{drinks[idx]} ordered. Price : {prices[idx]}won")
     total_price = total_price + prices[idx]
@@ -18,6 +22,9 @@ def order_process(idx: int):
 menu_lists = "".join([f"{k+1}) {drinks[k]} {prices[k]}won  " for k in range(len(drinks))])
 menu_lists = menu_lists + f"{len(drinks)+1}) Exit : "
 
+# help(abs)
+# help(len)
+# help(order_process)
 while True:
     menu = int(input(menu_lists))
     if len(drinks) >= menu >= 1:
