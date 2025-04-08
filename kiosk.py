c31b36b033e6ccaf5060abc2a7da75263f59b3f0
inhadeepblue <deepblue@inha.ac.kr>
2025-04-08 19:39:39 +0900

    week06 kiosk v2.7

--- kiosk.py
+++ kiosk.py
@@ -117,9 +117,24 @@ class OrderProcessor:
             print(f"{'No discount applied.':<30}")
             print(f"{'Total price:':<30} {self.total_price:>5} won")
 
+    def get_next_ticket_number(self) -> int:
+        """
+        Function that Produce next ticket number
+        :return: next ticket number
+        """
+        with open("ticket_number.txt", "r") as fp:
+            number = int(fp.read())
+
+        number = number + 1
+
+        with open("ticket_number.txt", "w") as fp:
+            fp.write(str(number))
+
+        return number
+
+
     def run(self):
         """Execute the order system"""
-
         while True:
             try:
                 menu_display = self.menu.display_menu()
@@ -137,3 +152,4 @@ class OrderProcessor:
                 print(e)  # Display the specific IndexError message
 
         self.print_receipt()
+        print(f"Queue number ticket : {self.get_next_ticket_number()}")
