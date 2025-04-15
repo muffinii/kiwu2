73ecda5a415362a03b83042fe8fbab90368d3d53
inhadeepblue <deepblue@inha.ac.kr>
2025-04-15 19:53:46 +0900

    week07 kiosk  v3.0 db

--- kiosk.py
+++ kiosk.py
@@ -1,4 +1,5 @@
 from typing import List  # type hint
+import sqlite3
 
 
 class Menu:
@@ -71,6 +72,20 @@ class OrderProcessor:
         self.amounts = [0] * menu.get_menu_length()
         self.total_price = 0
 
+        self.conn = sqlite3.connect('queue_number.db')
+        self.cur = self.conn.cursor()
+
+        self.cur.execute('''
+            create table if not exists ticket (
+            id integer primary key autoincrement,
+            number integer not null
+            )
+        ''')
+
+        self.conn.commit()
+
+
+
     def apply_discount(self, price: int) -> float:
         """
         Apply discount rate when the total amount exceeds a certain threshold
@@ -119,20 +134,20 @@ class OrderProcessor:
 
     def get_next_ticket_number(self) -> int:
         """
-        Function that Produce next ticket number
+        Function that Produce next ticket number (Database version)
         :return: next ticket number
         """
-        try:
-            with open("ticket_number.txt", "r") as fp:
-                number = int(fp.read())
-        except FileNotFoundError:
-            number = 0
+        self.cur.execute('select number from ticket order by number desc limit 1')
+        result = self.cur.fetchone()
 
-        number = number + 1
+        if result is None:
+            number = 1
+            self.cur.execute('insert into ticket (number) values (?)',(number,))
+        else:
+            number = result[0] + 1
+            self.cur.execute('insert into ticket (number) values (?)', (number,))
 
-        with open("ticket_number.txt", "w") as fp:
-            fp.write(str(number))
-
+        self.conn.commit()
         return number
 
     def run(self):
@@ -157,5 +172,5 @@ class OrderProcessor:
         print(f"Queue number ticket : {self.get_next_ticket_number()}")
 
     def __del__(self):
-        # db connection close ....
         print('End program')
+        self.conn.close()  # db connection close ....
