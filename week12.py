bcbe8508f90a31fe3403eb2291cae4e0a90022c7
inhadeepblue <deepblue@inha.ac.kr>
2025-05-20 18:55:50 +0900

    week12 kiosk v3.9

--- week12.py
+++ week12.py
@@ -1,18 +1,16 @@
 import tkinter as tk
 from kiosk import KioskGUI
-import requests
-
 
 if __name__ == "__main__":
-    url = f"https://wttr.in/incheon?&0&Q"
-    response = requests.get(url)
-    if response.status_code == 200:
-        print(response.text.strip())
-    else:
-        print(f"상태 코드 : {response.status_code}")
+    # url = f"https://wttr.in/incheon?&0&Q"
+    # response = requests.get(url)
+    # if response.status_code == 200:
+    #     print(response.text.strip())
+    # else:
+    #     print(f"상태 코드 : {response.status_code}")
 
-    menu_drinks = ["Ice Americano", "Cafe Latte", "Watermelon Juice", "Ice tea"]
-    menu_prices = [2000, 3000, 4900, 3500]
+    menu_drinks = ["Ice Americano", "Cafe Latte", "Watermelon Juice", "Ice tea", "A"]
+    menu_prices = [2000, 3000, 4900, 3500, 1]
 
     root = tk.Tk()
     app = KioskGUI(root, menu_drinks, menu_prices)
