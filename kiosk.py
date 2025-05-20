a3871cbac957d4016f5513f7e1b76cda696731bb
inhadeepblue <deepblue@inha.ac.kr>
2025-05-20 19:36:30 +0900

    week12 kiosk v4.1

--- kiosk.py
+++ kiosk.py
@@ -290,15 +290,20 @@ class KioskGUI:
     def update_weather_info(self) -> None:
         """ Load weather data from 'wttr.in'"""
         # url = "https://wttr.in/incheon?&0&Q"
-        url = "https://wttr.in/incheon?format=4"
+        # url = "https://wttr.in/incheon?format=4"  # ok
         # url = "https://naver.com/kim"  # 404
-        # url = "https://wttr123.in/incheon?format=4"
-        response = requests.get(url)
-        weather_text = response.text.strip()
-        if response.status_code == 200:
-            self.weather_label.config(text=f"Current weather ({weather_text})")
-        else:
-            self.weather_label.config(text=f"Weather information cannot be loaded. (Status code : {response.status_code})")
+        url = "https://wttr123.in/incheon?format=4"  # exception occur
+        # url = "https://www.nate.com"
+        try:
+            response = requests.get(url)  # exception occur
+            weather_text = response.text.strip()
+            if response.status_code == 200:
+                self.weather_label.config(text=f"Current weather ({weather_text})")
+            else:
+                self.weather_label.config(text=f"Weather information cannot be loaded. (Status code : {response.status_code})")
+        except Exception as err:
+            self.weather_label.config(text=f"Weather information error\n{err}")
+            # print(err)
 
 
     def add_to_order(self, idx: int) -> None:
