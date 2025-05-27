c2d788df7c8627af40dd12814e807233482b8211
inhadeepblue <deepblue@inha.ac.kr>
2025-05-27 18:51:04 +0900

    week13 kiosk v4.4 thread

--- thread01.py
+++ thread01.py
@@ -1,18 +1,26 @@
-# function based
 import threading
+import time
 
-def worker():
-    print("work")
+def print_numbers():
+    for i in range(1, 6):
+        print(f"Number: {i}")
+        time.sleep(1)
 
-t = threading.Thread(target=worker)
-t.start()
+def print_letters():
+    for letter in ['A', 'B', 'C', 'D', 'E']:
+        print(f"Letter: {letter}")
+        time.sleep(1.5)
 
-# class based
-# import threading
-#
-# class MyThread(threading.Thread):
-#     def run(self):
-#         print("work")
-# t = MyThread()
-# t.start()
-# t.join()
+# print_numbers()
+# print_letters()
+
+thread1 = threading.Thread(target=print_numbers)
+thread2 = threading.Thread(target=print_letters)
+
+thread1.start()
+thread2.start()
+
+thread1.join()
+thread2.join()
+
+print("All threads are done!")
